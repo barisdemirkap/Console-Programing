fde8f304a89d3958d5f07c3e5b1eece3822e983a
barisdemirkap <barisdemirkap@gmail.com>
2021-12-31 10:40:21 +0300

    Initial Program with C#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console_programing
+{
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+         Console.WriteLine("Hello, World!");
+         Console.WriteLine("Write your name");
+         string name=Console.ReadLine();
+         Console.WriteLine("Write your surname");
+         string surname=Console.ReadLine();
+
+         Console.WriteLine("Merhaba "+ name + " "+ surname);
+         
+         Console.ReadLine();
+
+    }
+}
+
+}
+
